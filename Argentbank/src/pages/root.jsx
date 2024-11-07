b6ab762b618c93dfee53888dd6894b8e2ae8dbf7
chatlapin <choh9323@gmail.com>
2024-11-07 22:32:00 +0100

    add Nav and Outlet

--- Argentbank/src/pages/root.jsx
+++ Argentbank/src/pages/root.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Nav from '../components/nav'
 
 export default function root() {
     return (
-        <div>root</div>
+        <>
+            <Nav />
+            <Outlet />
+        </>
     )
 }
